2ae7f1c96292b589e0740f0c6cf8802e4a0c2280
adamjace <adamjace@gmail.com>
2016-08-20 18:08:42 +1000

    html minify

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const minifyHTML = require('express-minify-html')
 const path = require('path')
 const logger = require('morgan')
 const cookieParser = require('cookie-parser')
@@ -10,6 +11,19 @@ const hbs = require('hbs')
 const fs = require('fs')
 
 const app = express()
+
+// minify
+app.use(minifyHTML({
+  override: true,
+  htmlMinifier: {
+    removeComments: true,
+    collapseWhitespace: true,
+    collapseBooleanAttributes: true,
+    removeAttributeQuotes: true,
+    removeEmptyAttributes: true,
+    minifyJS: true
+  }
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
@@ -29,7 +43,7 @@ app.use('/translate', translate)
 app.use('/sitemap.txt', sitemap)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found')
   err.status = 404
   next(err)
@@ -40,7 +54,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500)
     res.render('error', {
       message: err.message,
@@ -51,7 +65,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500)
   res.render('error', {
     message: err.message,
